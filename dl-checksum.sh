d88c36bd91173a2a6b844e3cd0534367a30043b2
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-02-01 16:06:15 -0500

    up to v2.1698-vsc1.41.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,31 +1,39 @@
 #!/usr/bin/env sh
-VER='1.1156-vsc1.33.1'
+set -e
 DIR=~/Downloads
 APPNAME=code-server
-MIRROR=https://github.com/cdr/$APPNAME/releases/download/$VER
+MIRROR=https://github.com/cdr/$APPNAME/releases/download
 
 dl()
 {
-    OS=$1
-    PLATFORM=$2
-    SUFFIX=$3
-    FILE=$APPNAME$VER-$OS-$PLATFORM.$SUFFIX
-    URL=$MIRROR/$FILE
-    LFILE=$DIR/$FILE
+    local ver=$1
+    local flavor_ver=$2
+    local os=$3
+    local arch=$4
+    local suffix=${5:-tar.gz}
+    local platform="${os}-${arch}"
+    local file="${APPNAME}${ver}-${flavor_ver}-${platform}.${suffix}"
+    local url=$MIRROR/$ver/$file
+    local lfile=$DIR/$file
 
-    if [ ! -e $LFILE ];
+    if [ ! -e $lfile ];
     then
-        wget -q -O $LFILE $URL
+        wget -q -O $lfile $url
     fi
 
-    printf "    # %s\n" $URL
-    printf "    %s-%s: sha256:%s\n" $OS $PLATFORM `sha256sum $LFILE | awk '{print $1}'`
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
 }
 
-printf "  '%s':\n" $VER
-dl darwin x64 zip
-dl linux x64 tar.gz
-dl musl x64 tar.gz
+dl_ver() {
+    local ver=$1
+    local flavor_ver=$2
+    printf "  '%s-%s':\n" $ver $flavor_ver
+    dl $ver $flavor_ver alpine arm64
+    dl $ver $flavor_ver alpine x86_64
+    dl $ver $flavor_ver darwin x86_64 zip
+    dl $ver $flavor_ver linux arm64
+    dl $ver $flavor_ver linux x86_64
+}
 
-
-
+dl_ver 2.1698 vsc1.41.1
